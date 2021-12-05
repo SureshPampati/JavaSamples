53759def7f5273e6da4e046db97f5239467796db
Guest-PC <psuresh9751@gmail.com>
2021-12-06 00:16:45 +0530

    Sample Java Codes

---
+++ src/main/java/Variable_Integer_to_Char.java
@@ -0,0 +1,11 @@
+public class Variable_Integer_to_Char {
+    public static void main(String[] args){
+        int num1 = 7890;
+        int num2 = 333;
+        // convert int to char
+        char a = (char)num1;
+        char b = (char)num2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
